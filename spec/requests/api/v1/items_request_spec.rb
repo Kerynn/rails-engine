11cae7936a052411ccf44a80e58d3765dc848f6d
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-06 17:56:34 -0800

    test: Add test for item create request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -56,4 +56,24 @@ RSpec.describe 'Items API' do
     expect(item[:data][:attributes]).to have_key(:unit_price)
     expect(item[:data][:attributes][:unit_price]).to be_a(Float)
   end
+
+  it 'can create a new item' do
+    merchant_id = create(:merchant).id
+    item_params = ({
+                    name: 'Hair Bender',
+                    description: 'Dark Roast, whole beans',
+                    unit_price: 8.99,
+                    merchant_id: merchant_id
+                  })
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last 
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
 end 
