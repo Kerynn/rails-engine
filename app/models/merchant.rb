03289c2ca66443204905bfba1104da2f05053902
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-07 15:14:50 -0800

    add find_merchant search method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,9 @@
 class Merchant < ApplicationRecord 
   has_many :items
+
+  def self.find_merchant(search)
+    where("lower(name) LIKE ?", 
+      sanitize_sql_for_conditions("%" + search.downcase + "%"))
+      .order(:name).first
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  before :each do 
+    @merchant_1 = create(:merchant, name: "World of Coffee")
+    @merchant_2 = create(:merchant, name: "Hello World")
+    @merchant_3 = create(:merchant, name: "Ring World")
+    @merchant_4 = create(:merchant, name: "Happy Place")
+  end
+
   describe 'relationships' do 
     it { should have_many :items }
   end
+
+  describe 'merchant search' do 
+    it 'will return a single merchant by alphabetical order if multiple results found' do 
+      expect(Merchant.find_merchant("World")).to eq(@merchant_2)
+    end
+
+    it 'can do a case-insensative search' do 
+      expect(Merchant.find_merchant("worLD")).to eq(@merchant_2)
+    end
+  end 
 end
