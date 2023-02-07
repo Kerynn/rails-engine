619d3ec5da044c8bd230544efd86ffa7f7385e17
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-06 17:02:09 -0800

    Add item show request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -26,4 +26,28 @@ RSpec.describe 'Items API' do
       expect(item[:attributes][:unit_price]).to be_a(Float)
     end
   end
+
+  it 'can show an item by its id' do 
+    id = create(:item).id 
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id]).to be_a(String)
+    string_comparison = id.to_s
+    expect(item[:data][:id]).to eq(string_comparison)
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+  end
 end 
