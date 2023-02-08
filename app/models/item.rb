593bf480a8ce9693db77c069b915f462d6c15149
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-08 12:45:29 -0800

    add delete invoice with single item method to item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,25 @@
 class Item < ApplicationRecord 
   belongs_to :merchant
-  has_many :invoice_items
+  has_many :invoice_items, dependent: :delete_all
   has_many :invoices, through: :invoice_items
   has_many :customers, through: :invoices
-  has_many :transactions, through: :invoices
 
   validates_presence_of :name, :description, :unit_price, :merchant_id
   validates_numericality_of :unit_price, :merchant_id
 
+  before_destroy :destroy_invoice_with_single_item, prepend: true
+  
   def self.find_all_items_search(search)
     # binding.pry
+  end
+
+  private 
+
+  def destroy_invoice_with_single_item
+    invoices.each do |invoice|
+      if invoice.items.count == 1
+        invoice.destroy
+      end
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Item, type: :model do
     it { should have_many :invoice_items }
     it { should have_many(:invoices).through(:invoice_items) }
     it { should have_many(:customers).through(:invoices) }
-    it { should have_many(:transactions).through(:invoices) }
   end
 
   describe 'validations' do 
@@ -17,6 +16,30 @@ RSpec.describe Item, type: :model do
     it { should validate_numericality_of :unit_price }
     it { should validate_numericality_of :merchant_id }
   end
+
+  describe 'destroy' do
+    it 'will destroy the invoice if the deleted item is only item on the invoice' do 
+      customer = Customer.create!(first_name: "Charlie", last_name: "Jones")
+      invoice1 = Invoice.create!(customer_id: customer.id, status: 2) # one item
+      invoice2 = Invoice.create!(customer_id: customer.id, status: 2) # two items
+      merchant = create(:merchant)
+      item1 = create(:item, merchant_id: merchant.id)
+      item2 = create(:item, merchant_id: merchant.id)
+      ii_1 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice1.id)
+      ii_2 = InvoiceItem.create!(item_id: item1.id, invoice_id: invoice2.id)
+      ii_3 = InvoiceItem.create!(item_id: item2.id, invoice_id: invoice2.id)
+
+      expect(Item.count).to eq(2)
+      expect(Invoice.count).to eq(2)
+      expect(InvoiceItem.count).to eq(3)
+
+      item1.destroy
+
+      expect(Item.count).to eq(1)
+      expect(Invoice.count).to eq(1)
+      expect(InvoiceItem.count).to eq(1)
+    end
+  end 
 
   describe 'find_all_items_search' do 
     before :each do 
