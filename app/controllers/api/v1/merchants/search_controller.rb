003a130ce8f3ac73a4891fa772414f28f9b8c399
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-07 19:20:47 -0800

    add bad request to render

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -7,11 +7,11 @@ class Api::V1::Merchants::SearchController < ApplicationController
         render json: MerchantSerializer.new(merchant)
       else
         err = SearchError.new("NOT FOUND", "No merchant matches search", 404)
-        render json: SearchErrorSerializer.new(err).serialized_error
+        render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :bad_request
       end
     else
       err = SearchError.new("NOT FOUND", "Name query must be entered", 404)
-      render json: SearchErrorSerializer.new(err).serialized_error
+      render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :bad_request
     end 
   end
 end
