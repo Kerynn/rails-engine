a90f1b780188088064c16f1cc6b75cd6e8bf01c4
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-07 18:34:22 -0800

    add sad path for find merchant search response

--- spec/requests/api/v1/merchant_find_request_spec.rb
+++ spec/requests/api/v1/merchant_find_request_spec.rb
@@ -9,12 +9,89 @@ RSpec.describe 'Merchant Search' do
   end
 
   it 'will return a single object in alphabetical order' do 
-    get '/api/v1/merchants/find'
+    get '/api/v1/merchants/find?name=World'
 
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant[:data][:id].to_i).to eq(@merchant_2.id)
+
+    expect(merchant).to have_key(:data)
+    expect(merchant[:data]).to be_a(Hash)
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to be_a(String)
+
+    expect(merchant[:data]).to have_key(:type)
+    expect(merchant[:data][:type]).to eq("merchant")
+
+    expect(merchant[:data]).to have_key(:attributes)
+    expect(merchant[:data][:attributes]).to be_a(Hash)
     
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 
-  xit 'can do a case_insensative search' do 
+  it 'will return the merchant with a case-insensative search' do 
+    get '/api/v1/merchants/find?name=woRLd'
 
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant[:data][:id].to_i).to eq(@merchant_2.id)
+  end
+
+  it 'will return an error message if no query params entered' do 
+    get '/api/v1/merchants/find?'
+
+    error_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(error_response).to have_key(:errors)
+    expect(error_response[:errors]).to be_an(Array)
+
+    expect(error_response[:errors][0]).to have_key(:status)
+    expect(error_response[:errors][0][:status]).to be_a(String)
+
+    expect(error_response[:errors][0]).to have_key(:message)
+    expect(error_response[:errors][0][:message]).to be_a(String)
+    
+    expect(error_response[:errors][0]).to have_key(:code)
+    expect(error_response[:errors][0][:code]).to be_an(Integer)
+  end
+
+  it 'will return an error message if query did not return a merchant result' do 
+    get '/api/v1/merchants/find?name=Goodbye'
+
+    error_response = JSON.parse(response.body, symbolize_names: true)
+   
+    expect(error_response).to have_key(:errors)
+    expect(error_response[:errors]).to be_an(Array)
+
+    expect(error_response[:errors][0]).to have_key(:status)
+    expect(error_response[:errors][0][:status]).to be_a(String)
+
+    expect(error_response[:errors][0]).to have_key(:message)
+    expect(error_response[:errors][0][:message]).to be_a(String)
+    
+    expect(error_response[:errors][0]).to have_key(:code)
+    expect(error_response[:errors][0][:code]).to be_an(Integer)
+
+    get '/api/v1/merchants/find?name='
+
+    error_response = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(error_response).to have_key(:errors)
+    expect(error_response[:errors]).to be_an(Array)
+
+    expect(error_response[:errors][0]).to have_key(:status)
+    expect(error_response[:errors][0][:status]).to be_a(String)
+
+    expect(error_response[:errors][0]).to have_key(:message)
+    expect(error_response[:errors][0][:message]).to be_a(String)
+    
+    expect(error_response[:errors][0]).to have_key(:code)
+    expect(error_response[:errors][0][:code]).to be_an(Integer)
   end
 end
