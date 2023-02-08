102314c20cb02524ff8f77b58eac666babd4cc4d
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-08 15:15:39 -0800

    remove text from merchant search

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -6,26 +6,12 @@ class Api::V1::Items::SearchController < ApplicationController
       if items != nil
         render json: ItemSerializer.new(items) 
       else 
-        err = SearchError.new("NOT FOUND", "Query must be entered", 404)
+        err = SearchError.new("NOT FOUND", "Query must be entered", 400)
         render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :bad_request
       end 
     else 
-      err = SearchError.new("NOT FOUND", "Query must be entered", 404)
-      render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :not_found   
+      err = SearchError.new("NOT FOUND", "Query must be entered", 400)
+      render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :bad_request   
     end
   end
 end
-
-  #   if params[:name]
-  #     merchant = Merchant.find_merchant(params[:name])
-  #     if merchant.class == Merchant 
-  #       render json: MerchantSerializer.new(merchant)
-  #     else
-  #       err = SearchError.new("NOT FOUND", "No merchant matches search", 404)
-  #       render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :bad_request
-  #     end
-  #   else
-  #     err = SearchError.new("NOT FOUND", "Name query must be entered", 404)
-  #     render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :bad_request
-  #   end 
-  # end
