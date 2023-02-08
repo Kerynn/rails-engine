ef4777a80664f9fbe3e5c9e5a5a107dcf316d041
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-08 08:49:47 -0800

    set up items find all route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
       namespace :merchants do 
         get '/find', to: 'search#show'
       end
+      namespace :items do 
+        get '/find_all', to: 'search#index'
+      end
       resources :merchants, only: [:index, :show]
       resources :items
       namespace :merchants do 
