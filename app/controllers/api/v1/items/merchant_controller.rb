b870a0c7bace4a6b6686583f5bd28eab2e4164e8
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-08 19:45:31 -0800

    comment out code not yet working in merchant items and item merchant

--- app/controllers/api/v1/items/merchant_controller.rb
+++ app/controllers/api/v1/items/merchant_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::Items::MerchantController < ApplicationController
 
   def show 
     item = Item.find(params[:id])
-    if item.id
-      render json: MerchantSerializer.new(item.merchant)
-    else 
-      render json: { errors: item.errors.full_messages.to_sentence }, status: :not_found
-    end
+    # if item.id
+    render json: MerchantSerializer.new(item.merchant)
+    # else 
+    #   render json: { errors: item.errors.full_messages.to_sentence }, status: :not_found
+    # end
   end
 end
--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -2,11 +2,11 @@ class Api::V1::Merchants::ItemsController < ApplicationController
   
   def index
     merchant = Merchant.find(params[:id])
-    if merchant.id
-      render json: ItemSerializer.new(merchant.items)
-    else
-      err = SearchError.new("NOT FOUND", "Merchant not found", 404)
-      render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :not_found
-    end 
+    # if merchant.id
+    render json: ItemSerializer.new(merchant.items)
+    # else
+    #   err = SearchError.new("NOT FOUND", "Merchant not found", 404)
+    #   render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :not_found
+    # end 
   end
 end
