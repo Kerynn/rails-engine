07bef43cdc1774ed8e7b2c10dbd937db6ee35ae9
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-08 18:28:47 -0800

    refactor to reduce index method

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,30 +2,21 @@ class Api::V1::Items::SearchController < ApplicationController
 
   def index 
     if params[:name] && (params[:min_price] || params[:max_price])
-      err = SearchError.new("NOT FOUND", "Cannot query name and price", 400)
-      render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :bad_request   
+      render json: { errors: SearchErrorSerializer.new(name_and_price_query).serialized_error }, status: :bad_request   
     elsif params[:name]
-      name_items = Item.find_all_items_name(params[:name])
       if name_items != nil
         render json: ItemSerializer.new(name_items) 
       else 
-        err = SearchError.new("NOT FOUND", "Query must be entered", 400)
-        render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :bad_request
+        render json: { errors: SearchErrorSerializer.new(no_query).serialized_error }, status: :bad_request
       end 
     elsif params[:min_price] && params[:max_price]
-      min_items = Item.find_all_items_min_price(params[:min_price].to_f)
-      max_items = Item.find_all_items_max_price(params[:max_price].to_f)
-      all_items = min_items + max_items
       render json: ItemSerializer.new(all_items)
     elsif params[:min_price]
-       min_items = Item.find_all_items_min_price(params[:min_price].to_f)
        render json: ItemSerializer.new(min_items)
     elsif params[:max_price] 
-      max_items = Item.find_all_items_max_price(params[:max_price].to_f)
       render json: ItemSerializer.new(max_items)
     else 
-      err = SearchError.new("NOT FOUND", "Query must be entered", 400)
-      render json: { errors: SearchErrorSerializer.new(err).serialized_error }, status: :bad_request   
+      render json: { errors: SearchErrorSerializer.new(no_query).serialized_error }, status: :bad_request   
     end
   end
 
@@ -41,5 +32,17 @@ class Api::V1::Items::SearchController < ApplicationController
 
   def max_items 
     Item.find_all_items_max_price(params[:max_price].to_f)
+  end
+
+  def all_items
+    min_items + max_items
+  end
+
+  def no_query 
+    SearchError.new("NOT FOUND", "Query must be entered", 400)
+  end
+
+  def name_and_price_query 
+    SearchError.new("NOT FOUND", "Cannot query name and price", 400)
   end
 end
